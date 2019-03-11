187dfd5a7d2521bd7f493ec4e624daf95194d098
Isha Geurtsen <139a414@gmail.com>
2019-03-11 16:07:01 +0100

    improved names and increased documentation

--- code/src/app.cpp
+++ code/src/app.cpp
@@ -1,10 +1,17 @@
 #include <app.hpp>
 #include <array>
 
-constexpr unsigned int size = 4000 * 4;
-std::array<bool, size> buffer;
-unsigned int index = 0;
+// sampling duration in seconds
+constexpr unsigned int sampling_duration = 4;
+// frequency in hertz
+constexpr unsigned int frequency = 4'000;
+// storage size
+constexpr unsigned int sample_size = frequency * sampling_duration;
+// sampeling period in microseconds
+constexpr unsigned int period = 1'000'000 / frequency;
 
+std::array<bool, sample_size> sample_buffer;
+unsigned int index = 0;
 
 r2d2::app_c::app_c(hwlib::target::pins sensor_pin,
                    hwlib::target::pins status_pin)
@@ -13,18 +20,18 @@ r2d2::app_c::app_c(hwlib::target::pins sensor_pin,
 
 void r2d2::app_c::main() {
     while (true) {
-        if (index == size) {
+        if (index == sample_size) {
             r2d2::app_c::publish();
             r2d2::app_c::idle();
         }
-        buffer[index++] = sensor.read();
+        sample_buffer[index++] = sensor.read();
         status.write(sensor.read());
-        hwlib::wait_us(250);
+        hwlib::wait_us(period);
     }
 }
 
 void r2d2::app_c::publish() {
-    for (const auto &b : buffer) {
+    for (const auto &b : sample_buffer) {
         hwlib::cout << b;
     }
     hwlib::cout << "\n";
