858488ebf9dd5f2e4a728b6cfced59015506ff74
lldiegon <lldiegon@gmail.com>
2019-03-31 17:36:19 +0200

    initial commit

--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,11 @@
-#include <hwlib.hpp>
-#include <app.hpp>
+#include "hwlib.hpp"
 
 int main(void) {
-    // kill the watchdog
+  // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
-
-    auto app = r2d2::app_c(hwlib::target::pins::d2, hwlib::target::pins::d13);
-    app.main();
+    for (;;){
+        hwlib::cout << "this works via arduino";
+        hwlib::wait_ms(1000);
+    }
 }
