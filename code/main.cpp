f0f869058a57f745170b74ad068275e2e3bed3e1
lldiegon <lldiegon@gmail.com>
2019-03-31 17:46:13 +0200

    trying to read the mic_pin

--- code/main.cpp
+++ code/main.cpp
@@ -3,9 +3,13 @@
 int main(void) {
   // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
+
+    auto pin_mic = hwlib::target::pin_in_out(hwlib::target::pins::a3);
+
     hwlib::wait_ms(1000);
     for (;;){
         hwlib::cout << "this works via arduino";
+        hwlib::cout << pin_mic.read();
         hwlib::wait_ms(1000);
     }
 }
