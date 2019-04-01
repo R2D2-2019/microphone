61505ff8b973539af4955ad0620264aaa9e0e364
lldiegon <lldiegon@gmail.com>
2019-04-01 14:41:13 +0200

    Testing the arduino adc in combination with the mic

--- code/main.cpp
+++ code/main.cpp
@@ -1,15 +1,21 @@
 #include "hwlib.hpp"
+#include <hwlib-arduino-due.hpp>
 
 int main(void) {
   // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
 
-    auto pin_mic = hwlib::target::pin_in_out(hwlib::target::pins::a3);
+    //Testing to read input data using pin_in as well as using the adc
+    auto pin_mic = hwlib::target::pin_in(hwlib::target::pins::a3);
+    //Constructor to define the pin as a adc input
+    auto adc = hwlib::target::pin_adc( hwlib::target::ad_pins::a1 ); //Object creation
+
 
     hwlib::wait_ms(1000);
     for (;;){
         hwlib::cout << "this works via arduino";
         hwlib::cout << pin_mic.read();
+        unsigned int mic_value = adc.get(); //Convert and read	
         hwlib::wait_ms(1000);
     }
 }
