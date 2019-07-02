c3263443778c3852ebbfa463cd82618754b4568e
56KbModem <nick-snel@hotmail.com>
2019-07-02 13:45:38 +0200

    added doxygen, sampling functionality

--- code/headers/microphone_controller.hpp
+++ code/headers/microphone_controller.hpp
@@ -5,31 +5,48 @@
 namespace r2d2::microphone {
 
     /**
-     * Class steering_wheel_controller_c provides functions
-     * for reading the data of the trust steering wheel.
+     * Class microphone_controller_c provides functions
+     * for collecting audio data from the KY-038 module.
      */
     class microphone_controller_c {
     private:
         hwlib::target::pin_adc &mic;
-        bool mute = false;
+	uint16_t sample_buffer[63]; // max internal CAN bus buffer size
+        bool muted = false; // microphone is not muted by default
 
     public:
         /**
          * Instantiate the microphone controller class
          *
-         * @param mic The adc input that reads the microphone.
+         * @param mic The adc input that reads the microphone
          */
         microphone_controller_c(hwlib::target::pin_adc &new_mic):
 	    mic(new_mic)
 	{}
 
         /**
-         * This function overrides the read() function of pin_in
-         * and reads the data of a pin_in.
+         * This function reads the analog data from the
+	 * microphone pin
          *
-         * @internal refreshes/reads the incoming data.
+         * @return returns the microphone value
+	 * 
          */
-        int_fast16_t read_mic();
+        uint16_t read_mic();
+
+	/**
+	 * This function returns a pointer to the sample buffer
+	 *
+	 * @return pointer to sample buffer 
+	 */
+	uint16_t *read_buffer();
+
+        /**
+	 * This function takes a block of 63
+	 * samples and collects it in it's internal buffer
+	 * 
+	 * @internal calls read_mic() to fill sample_buffer
+	 */
+	void sample();
 
         /**
          * This function blocks the read_mic() function
@@ -48,7 +65,7 @@ namespace r2d2::microphone {
 
         /**
          * This function is for testing/reading
-         * the incoming data of the steering wheel.
+         * the incoming data of the microphone.
          */
         void print();
     };
--- code/src/microphone_controller.cpp
+++ code/src/microphone_controller.cpp
@@ -1,33 +1,40 @@
-#include <microphone_controller.hpp>
+#include "../headers/microphone_controller.hpp"
 
-namespace r2d2::microphone{
-
-    int_fast16_t r2d2::microphone::microphone_controller_c::read_mic() {
-        if (!mute){
-            return microphone_controller_c::mic.read(); // return adc pin (microphone) value
-        }
-        else{
-            return 0; // don't return an audible value
-        }
+uint16_t r2d2::microphone::microphone_controller_c::read_mic() {
+    if (!muted){
+        return r2d2::microphone::microphone_controller_c::mic.read(); // return adc pin (microphone) value
     }
-
-    void r2d2::microphone::microphone_controller_c::mute() {
-        if (!mute){ // if the mic is not muted
-            mute = true; // mute the microphone
-        }
-        return;
-    
-    }   
-
-    void r2d2::microphone::microphone_controller_c::unmute(){
-        if (mute){ // if the mic is muted
-            mute = false; // unmute the microphone;
-        }
-        return;
+    else{
+        return 0; // don't return an audible value
     }
-
-    void r2d2::microphone::microphone_controller_c::print() {
-        hwlib::cout << "[Mic level]: " << read_mic() << '\n';
-    }
-
 }
+
+void r2d2::microphone::microphone_controller_c::sample(){
+    for (int i=0; i < 63; ++i){
+        sample_buffer[i] = microphone_controller_c::read_mic(); // fill sample buffer
+    }
+}
+
+uint16_t* r2d2::microphone::microphone_controller_c::read_buffer(){
+    return sample_buffer; // return pointer to buffer
+}
+
+void r2d2::microphone::microphone_controller_c::mute() {
+    if (!muted){ // if the mic is not muted
+        muted = true; // mute the microphone
+    }
+    return;
+    
+}
+
+void r2d2::microphone::microphone_controller_c::unmute(){
+    if (muted){ // if the mic is muted
+        muted = false; // unmute the microphone;
+    }
+    return;
+}
+
+void r2d2::microphone::microphone_controller_c::print() {
+    hwlib::cout << "[Mic level]: " << read_mic() << '\n';
+}
+
