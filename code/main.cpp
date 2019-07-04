7d614ae1e2c77916af99988406db5783f48f17b9
56KbModem <nick-snel@hotmail.com>
2019-07-04 19:34:19 +0200

    fix hwlib namespace issue

--- code/main.cpp
+++ code/main.cpp
@@ -17,7 +17,7 @@ int main(void) {
     hwlib::wait_ms(10);
 
     comm_c comm;
-    auto mic = hwlib::target::pin_adc(hwlib::target::ad_pins::a0);
+    auto mic = hwlib::pin_adc(hwlib::target::ad_pins::a0);
     auto microphone_controller = microphone::microphone_controller_c(mic);
 
     microphone::module_c module(comm, microphone_controller);
